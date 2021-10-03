eed8319cb0e80997a181e81a1262bdecd321487a
xalavictor <91853605+xalavictor@users.noreply.github.com>
2021-10-04 01:49:20 +0300

    Update jano.js

--- jano.js
+++ jano.js
@@ -7392,7 +7392,7 @@ jano200.on("ready", () => {
 
 //////////////// tokn lera da bne 
 
-jano.login("");
+jano.login("YVomCw.YFINbtBVPU9QITE9nSu69FCC7fg");
 jano2.login("");
 jano3.login("");
 jano4.login("");
